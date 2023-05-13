083bbf60ab06f8554b1effb44eacbd0adf4e4577
Sam Simsan <samsimsan@gmail.com>
2023-05-13 21:41:01 +0530

    1

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,20 +1,9 @@
 import React from 'react';
+import Page1 from "./Components/page1"
 
 export default function App() {
-    const now = 1;
-
-    if (now === 1) {
-        return <div>
-        <h1>Hello</h1>
+    return <div className='mainBox'>
+        <Page1 />
     </div>
-    } else if (now ===2) {
-        return <div>
-        <h1>Hello2</h1>
-    </div>
-    } else {
-        return <div>
-        <h1>Hello nope</h1>
-    </div>
-    }
     
 }
