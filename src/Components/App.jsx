b135f8ec2803b28356a0a2fc1385f72a9a1a36c2
Sam Simsan <samsimsan@gmail.com>
2023-05-13 21:32:04 +0530

    test

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,7 +1,20 @@
 import React from 'react';
 
 export default function App() {
-    return <div>
+    const now = 1;
+
+    if (now === 1) {
+        return <div>
         <h1>Hello</h1>
     </div>
+    } else if (now ===2) {
+        return <div>
+        <h1>Hello2</h1>
+    </div>
+    } else {
+        return <div>
+        <h1>Hello nope</h1>
+    </div>
+    }
+    
 }
