a6d332b89ba81e9db008eddf7dca5c87663580a1
Sam Simsan <samsimsan@gmail.com>
2023-06-01 16:14:06 +0530

    checkbutton

--- src/Components/Buttons/Button2.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-export default function Button2(props) {
-
-    return <button onClick={() => {
-        props.Clicked((props.pageNum + 1) % 5)
-    }} className={props.class}>{props.text}</button>
-}
---
+++ src/Components/Buttons/CheckedButton.jsx
@@ -0,0 +1,11 @@
+const CheckedButton = () => {
+    return (
+      <label class="switch">
+        <input type="checkbox" />
+        <span class="slider round"></span>
+      </label>
+    );
+  };
+  
+  export default CheckedButton;
+  
